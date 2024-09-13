ce1aadeb62aa8c7299236dd7eb35a80f75028df3
odiegosilva1 <odiegosilvadev@gmail.com>
2024-09-13 14:20:19 -0300

    Aula 1 ao 6

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Escreen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("O ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+
+        double media = (9.8 + 5.3 + 7.3) / 3;
+
+        System.out.println(media);
+    }
+}
