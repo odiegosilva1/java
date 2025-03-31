274af71db7a26c90c6c0ac7dd840d4eb875c95ef
odiegosilva1 <odiegosilvadev@gmail.com>
2025-03-31 16:12:33 -0300

    create: claasse-Livro

---
+++ livros-classe/Livro.java
@@ -0,0 +1,7 @@
+// crieando a classe livro
+public class Livro {
+  String titulo, autor, genero;
+  int qtdPaginas;
+  double preco;
+
+}
---
+++ livros-classe/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+
+    Livro livro1 = new Livro();
+    Livro livro2 = new Livro();
+
+    livro1.titulo = "Harry Potter";
+
+  }
+
+}
