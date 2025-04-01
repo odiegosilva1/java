89d9579e1543ae56f2b95b8905898e9f92e3f7fe
odiegosilva1 <odiegosilvadev@gmail.com>
2025-04-01 15:49:44 -0300

    add: Método

--- livros-classe/Livro.java
+++ livros-classe/Livro.java
@@ -4,4 +4,20 @@ public class Livro {
   int qtdPaginas;
   double preco;
 
+  // Métodos em java
+  void mostrarDadosLivro() {
+    System.out.println("Dados dos Livros" + titulo + ":");
+    System.out.println("\tAutor " + autor);
+    System.out.println("\tGênero " + genero);
+    System.out.println("\tPáginas " + qtdPaginas);
+    System.out.println("\tPreço " + preco);
+  }
+
+  void calculaPreçoLivro() {
+    preco = qtdPaginas / 3;
+    System.out.println("Novo preço do livro: " + titulo + ":" + preco);
+  }
+
 }
+
+// Quando dentro do IF existrie somente uma instrução não precisa usar "{}"
