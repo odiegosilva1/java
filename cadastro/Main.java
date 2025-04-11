72d6a8bce1af9f61c28d37d1d07250f3fe394b4c
odiegosilva1 <odiegosilvadev@gmail.com>
2025-04-11 14:42:51 -0300

    update: Main.java

---
+++ cadastro/Main.java
@@ -0,0 +1,8 @@
+package cadastro;
+
+public class Main {
+  public static void main(String[] args) { // Isso define por onde começa o preograma.
+
+  }
+
+}
---
+++ cadastro/User.java
@@ -0,0 +1,13 @@
+package cadastro;
+
+// Atributos
+public class User {
+  String name, email, password;
+  double salary;
+
+}
+
+  // Métodos - é void pois o valor é definido na pariemeira parte.
+  void CalculateSalary(int HoursMonth, double HourlyRate) {
+
+}
