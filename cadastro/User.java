561cffb3658f35440926bd1410bad4dd09d56272
odiegosilva1 <odiegosilvadev@gmail.com>
2025-04-11 21:43:44 -0300

    commit:Main.java

--- cadastro/User.java
+++ cadastro/User.java
@@ -2,12 +2,24 @@ package cadastro;
 
 // Atributos
 public class User {
-  String name, email, password;
-  double salary;
+  public String name, email, password;
+  public double salary;
 
-}
+ }
 
   // Métodos - é void pois o valor é definido na pariemeira parte.
-  void CalculateSalary(int HoursMonth, double HourlyRate) {
+  public void CalculateSalary(int HoursMonth, double HourlyRate) {
+    salary = HoursMonth * HourlyRate;
 
-}
+  }
+
+  // Mostra na tela informações do usuário
+  public void ShowInfo() {
+    System.out.println("Dados usuário " + name + ":");
+    System.out.println("E-mail" + email);
+    System.out.println("Senha" + password);
+
+  }
+
+// Encapsulamento e definir quais parets do código vão ser visiveis de dentro e
+// de fora da classe.
